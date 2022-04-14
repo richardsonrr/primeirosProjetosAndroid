b29e74a9e79be84e6033b105805603f7cec4fbfd
Richardson Rodrigues <richardsonrodrigues.dev@gmail.com>
2022-04-14 16:01:54 -0300

    Implementação de RecyclerView

--- OrgsAlura/app/src/main/java/br/com/rrdev/orgs/ui/activity/MainActivity.kt
+++ OrgsAlura/app/src/main/java/br/com/rrdev/orgs/ui/activity/MainActivity.kt
@@ -2,9 +2,12 @@ package br.com.rrdev.orgs.ui.activity
 
 import android.app.Activity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.rrdev.orgs.R
+import br.com.rrdev.orgs.model.Produtos
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 class MainActivity : Activity(){
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,10 +18,22 @@ class MainActivity : Activity(){
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         //depois usamos o componente padrao do recyler para fazer as configurações
         // o chamado adpter, deve se mandar uma referencia basica propria pro recycler
-        recyclerView.adapter  = br.com.rrdev.orgs.adpter.ListaProdutosAdapter()
+
+
+        recyclerView.adapter  = br.com.rrdev.orgs.adpter.ListaProdutosAdapter(context = this, produtos =
+        listOf(
+            Produtos(
+                nome = "Abacaxi", descrição = "Abacaxi amarelo grande", valor = BigDecimal("13.9")
+            ),
+            Produtos(
+                nome = "Laranja", descrição = "laranja grande", valor = BigDecimal(12.99).setScale(2,RoundingMode.UP)
+            )
+
+        ))
         // somos obrigados a implementar uma class para referenciar esse adpter quando
         //criamos um reyclerView
 
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
 
     }
