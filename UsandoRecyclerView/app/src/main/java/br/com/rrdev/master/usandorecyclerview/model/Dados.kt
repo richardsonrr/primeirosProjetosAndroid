95c90db76b71526a0b9674c91bec5844e2c1d6fe
Richardson Rodrigues <71666036+richardsonrr@users.noreply.github.com>
2022-04-15 18:17:58 -0300

    Delete UsandoRecyclerView directory
    diretorio repetido

--- UsandoRecyclerView/app/src/main/java/br/com/rrdev/master/usandorecyclerview/model/Dados.kt
+++
@@ -1,9 +0,0 @@
-package br.com.rrdev.master.usandorecyclerview.model
-
-data class Dados(
-    val title: String,
-    val author: String,
-    val thumbnailUrl:String,
-    val link:String
-
-    )
