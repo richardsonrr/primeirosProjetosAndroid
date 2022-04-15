8dac0604397ec1fc70ced23e6d40deb18703411f
Richardson Rodrigues <richardsonrodrigues.dev@gmail.com>
2022-04-14 22:08:19 -0300

    Init UsandorRecyclerView

---
+++ UsandoRecyclerView/app/src/main/java/br/com/rrdev/master/usandorecyclerview/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.rrdev.master.usandorecyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
