d964f82b3490f5720709938ffe8c4f5605333555
Richardson Rodrigues <richardsonrodrigues.dev@gmail.com>
2022-04-16 23:03:05 -0300

    Add novo layout adicionarProdutos

---
+++ OrgsAlura/app/src/main/java/br/com/rrdev/orgs/ui/activity/FormularioDeCadastroActivity.kt
@@ -0,0 +1,11 @@
+package br.com.rrdev.orgs.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.rrdev.orgs.R
+
+class FormularioDeCadastroActivity : AppCompatActivity(R.layout.formulario_de_cadastro) {
+
+
+
+}
