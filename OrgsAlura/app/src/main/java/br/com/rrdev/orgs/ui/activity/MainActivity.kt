79948b20091a5d1cd1882b36c765743f67cc038c
Richardson Rodrigues <richardsonrodrigues.dev@gmail.com>
2022-04-17 17:12:32 -0300

    update gitIgnore

--- OrgsAlura/app/src/main/java/br/com/rrdev/orgs/ui/activity/MainActivity.kt
+++ OrgsAlura/app/src/main/java/br/com/rrdev/orgs/ui/activity/MainActivity.kt
@@ -1,15 +1,10 @@
 package br.com.rrdev.orgs.ui.activity
 
-import android.app.Activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.rrdev.orgs.R
 import br.com.rrdev.orgs.dados.dados
-import br.com.rrdev.orgs.model.Produtos
-import java.math.BigDecimal
-import java.math.RoundingMode
 
 class MainActivity : AppCompatActivity(R.layout.activity_main){
 
