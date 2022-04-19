cd39d571c70054fd28d18db72d5763113665c201
Richardson Rodrigues <richardsonrodrigues.dev@gmail.com>
2022-04-19 11:35:09 -0300

    Add ViewBinding no adapter

--- OrgsAlura/app/src/main/java/br/com/rrdev/orgs/adpter/ListaProdutosAdapter.kt
+++ OrgsAlura/app/src/main/java/br/com/rrdev/orgs/adpter/ListaProdutosAdapter.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import br.com.rrdev.orgs.R
+import br.com.rrdev.orgs.databinding.ProductItemBinding
 import br.com.rrdev.orgs.model.Produtos
 
 // tal class obrigatoriamente que é com base em uma abstract tem que sobrescrever
@@ -23,14 +24,15 @@ class ListaProdutosAdapter(
     //devemos criar uma class ViewHolder() que é uma definição generica pra que possamos
     //criar o nosso proprio
     //ele recebe uma View
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    class ViewHolder(private val binding: ProductItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
         fun vincula(produto: Produtos) {
 
-            val product = itemView.findViewById<TextView>(R.id.product)
+            val product = binding.product
             product.text = produto.nome
-            val descricao = itemView.findViewById<TextView>(R.id.description)
+            val descricao = binding.description
             descricao.text = produto.descrição
-            val valor = itemView.findViewById<TextView>(R.id.valor)
+            val valor = binding.valor
             valor.text = produto.valor.toPlainString()
         }
     }
@@ -39,9 +41,10 @@ class ListaProdutosAdapter(
     //ViewHolder responsavel por pegar cada uma dessas views e fazer o processo de bind
     //refazer elas na tela na estrutura
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val inflater = LayoutInflater.from(context)
-        val view = inflater.inflate(R.layout.product_item, parent, false)
-        return ViewHolder(view)
+        val binding = ProductItemBinding.inflate(LayoutInflater.from(context),parent,false)
+
+        return ViewHolder(binding)
+
     }
 
     //Vai indicar em qual momento dos itens do Recycler estamos e qual sua posição e oq queremos
